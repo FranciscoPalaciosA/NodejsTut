77126ff3fe1b2c919fd5c2f16195d69f4a1d5c8c
FranciscoPalaciosA <f.palacios.arriaga@gmail.com>
2018-10-02 10:50:42 -0500

    Chapter 7

--- modules_require.js
+++ modules_require.js
@@ -1,3 +1,6 @@
-var counter = require('./count');
+var stuff = require('./stuff');
 
-console.log(counter([1, 2, 3]));
+console.log(stuff.counter([1, 2, 3]));
+console.log(stuff.adder(5, 10));
+console.log(stuff.adder(stuff.pi, 10));
+console.log(stuff.pi);
