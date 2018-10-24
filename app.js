b317bed20e9f755e3cb908c2df823a05ae1556b7
FranciscoPalaciosA <f.palacios.arriaga@gmail.com>
2018-10-23 22:59:06 -0500

    Chapter 24

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.get('/contact', function(req, res){
   res.send('<h1>Contact Page</h1>');
 });
 
+app.get('/profile/:id', function(req, res){
+  res.send('You requested to see a profile with the id of ' + req.params.id)
+});
+
 app.listen(3000);
